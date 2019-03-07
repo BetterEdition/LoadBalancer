58385cecdd7fa6e794efcfe640911b587b99f405
Jesper <jesperenemark@hotmail.com>
2019-03-07 01:02:38 +0100

    Loadbalancer working

---
+++ LoadBalancerRemade/Controllers/RequestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LoadBalancerRemade.Models;
+namespace LoadBalancerRemade.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        RequestAndResponseHandler RnRHandler = new RequestAndResponseHandler();
+        // GET: api/Request
+        [Route("{num1}/{num2}")]
+        public Task<long> get(long num1, long num2)
+        {
+
+            var Response = RnRHandler.sendRequestToService(num1, num2);
+
+            return Response;
+        }
+    }
+}
---
+++ LoadBalancerRemade/Models/RequestAndResponseHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Diagnostics;
+namespace LoadBalancerRemade.Models
+{
+    public class RequestAndResponseHandler
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static Queue<string> Servers = new Queue<string>(new[] { "http://localhost:5005/api/primenum/", "http://localhost:5006/api/primenum/" });
+        public async Task<long> sendRequestToService(long num1, long num2)
+        {
+
+
+            // Get the next one up
+            String currentServer = Servers.Dequeue();
+
+            // Adds the server to the end of the queue
+            Servers.Enqueue(currentServer);
+
+            Console.WriteLine("HALLO I AM A UNICORN");
+            // Gives back a response from the current server requested to
+            var response = await client.GetStringAsync(currentServer + num1 + "/" + num2);
+            // Console for debug
+            String time = DateTime.Now.ToString("h:mm:ss tt");
+
+            Debug.WriteLine(currentServer + " " + time);
+
+
+            return Convert.ToInt64(response);
+
+        }
+    }
+
+}
